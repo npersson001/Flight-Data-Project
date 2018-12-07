e13f29d29c1a981b32b126032a2d52e878546ac5
npersson001 <npers@DESKTOP-OC3IGJT.wireless-1x.unc.edu>
2018-12-07 12:23:28 -0500

    added framework for registering a new user

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var pass;
 
 $(document).ready(() => {
 	alert("script starting");
-  
+
   $('#login_btn').on('click', () => {
 
 		user = $('#login_user').val();
@@ -18,15 +18,29 @@ $(document).ready(() => {
   });  
 
   $('#register_btn').on('click', () => {
+    alert("pressed register button");
+
+    build_register_interface();
 
     user = $('#login_user').val();
-    pass = $('#login_pass').val();
-
-    console.log(user);
-    console.log(pass);
-    // alert("presssed");
+    pass1 = $('#login_pass').val();
+    pass2 = $('#login_pass').val();
+    
     
     // ajax call to register a new user
   }); 
 });
+
+var build_register_interface = function () {
+  let body = $('body');
+
+  body.empty();
+  body.append('<h1>Register New User</h1>' +
+      '<div id="register_div">' +
+      'New username: <input type="text" id="reg_user"><br>' +
+      'New password: <input type="text" id="reg_pass"><br>' +
+      'Confirm password: <input type="text" id="reg_conf"><br>' +
+      '<button id="submit_registration_btn">Register</button>' + 
+      '</div>');
+}
 
