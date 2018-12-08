c74ca3dbbaa6fd221c29b9bdcbfa1497cdeb30d9
npersson001 <npers@DESKTOP-OC3IGJT.wireless-1x.unc.edu>
2018-12-08 11:49:58 -0500

    implemented login, register still giving unprocessable error

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
-var root_url = " http://comp426.cs.unc.edu:3001/api/";
+var root_url = " http://comp426.cs.unc.edu:3001/";
 var user;
 var pass;
 
 $(document).ready(() => {
 	alert("script starting");
 
-  $('#login_btn').on('click', () => {
+  $(document).on('click', '#login_btn', () => {
+    alert("attempting login");
 
 		user = $('#login_user').val();
 		pass = $('#login_pass').val();
+
+    alert(user);
+    alert(pass);
 
 		console.log(user);
 		console.log(pass);
 		// alert("presssed");
 		
-		// ajax call to login
+		$.ajax(root_url + 'sessions', {
+      type: 'POST',
+      dataType: 'json',
+      // contentType: 'application/json',
+      xhrFields: {withCredentials: true},
+      data: {
+        'user': {
+          'username': user,
+          'password': pass
+        }
+      },
+      success: (response) => {
+        build_flight_interface(); // consider returning to the login screen here
+      },
+      error: () => {
+        alert('Login failed!');
+      }
+    });
   });  
 
   $('#register_btn').on('click', () => {
     alert("pressed register button");
-
     build_register_interface();
-
-    user = $('#login_user').val();
-    pass1 = $('#login_pass').val();
-    pass2 = $('#login_pass').val();
-    
-    
-    // ajax call to register a new user
   }); 
+
+  $(document).on('click', '#submit_registration_btn', () => {
+    alert("pressing sumbitting registration button");
+
+    // get user info from text boxes
+    user = $('#reg_user').val();
+    pass1 = $('#reg_pass').val();
+    pass2 = $('#reg_conf').val();
+
+    // make sure no empty strings and that the passwords match
+    if(user == "" || pass1 == "" || pass2 == ""){
+      alert("Must give input in all textboxes!");
+    }
+    else if(pass1 != pass2){
+      alert("Passwords do not match, try again!")
+    }
+    // otherwise register the user, note that we dont check if the user already exists
+    else{
+      $.ajax(root_url + 'users', {
+        type: 'POST',
+        dataType: 'json',
+        // xhrFields: {withCredentials: true},
+        data: {
+          'user': {
+            'username': user,
+            'password': pass
+          }
+        },
+        success: (response) => {
+          build_flight_interface(); // consider returning to the login screen here
+        },
+        error: () => {
+          alert('Registering user failed!');
+        }
+      });
+    }
+  });
+
 });
 
 var build_register_interface = function () {
@@ -37,10 +88,13 @@ var build_register_interface = function () {
   body.empty();
   body.append('<h1>Register New User</h1>' +
       '<div id="register_div">' +
-      'New username: <input type="text" id="reg_user"><br>' +
-      'New password: <input type="text" id="reg_pass"><br>' +
+      'Username: <input type="text" id="reg_user"><br>' +
+      'Password: <input type="text" id="reg_pass"><br>' +
       'Confirm password: <input type="text" id="reg_conf"><br>' +
       '<button id="submit_registration_btn">Register</button>' + 
       '</div>');
 }
 
+var build_flight_interface = function () {
+  alert("building flight interface");
+}
